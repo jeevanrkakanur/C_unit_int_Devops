b6a005517f3b3bce604eb82196ec26c7dba780cf
jeevanrkakanur <jeevankakanur2445@gmail.com>
2026-04-16 15:18:21 +0530

    updated

--- src/calculator.c
+++ src/calculator.c
@@ -1,3 +1,17 @@
+#ifndef CALCULATOR_H
+
+#define CALCULATOR_H
+ 
+int add(int a, int b);
+
+int subtract(int a, int b);
+
+int multiply(int a, int b);
+
+float divide(int a, int b);
+ 
+#endif
+ 
 #include "calculator.h"
  
 int add(int a, int b) {
@@ -12,5 +26,21 @@ int subtract(int a, int b) {
 
 }
  
+int multiply(int a, int b) {
 
+    return a * b;
+
+}
+ 
+float divide(int a, int b) {
+
+    if (b == 0) {
+
+        return 0; // simple handling
+
+    }
+
+    return (float)a / b;
+
+}
  
