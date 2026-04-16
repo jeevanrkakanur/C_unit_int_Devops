28dd5321374960bc90d64a99065bbf92537fe720
jeevanrkakanur <jeevankakanur2445@gmail.com>
2026-04-16 15:19:23 +0530

    update

--- src/calculator.c
+++ src/calculator.c
@@ -1,46 +1,20 @@
-#ifndef CALCULATOR_H
-
-#define CALCULATOR_H
- 
-int add(int a, int b);
-
-int subtract(int a, int b);
-
-int multiply(int a, int b);
-
-float divide(int a, int b);
- 
-#endif
- 
 #include "calculator.h"
  
 int add(int a, int b) {
-
     return a + b;
-
 }
  
 int subtract(int a, int b) {
-
     return a - b;
-
 }
  
 int multiply(int a, int b) {
-
     return a * b;
-
 }
  
 float divide(int a, int b) {
-
     if (b == 0) {
-
         return 0; // simple handling
-
     }
-
     return (float)a / b;
-
 }
- 
