6b57d1b8efce2c763a15f90f068e397566928a8b
jeevanrkakanur <jeevankakanur2445@gmail.com>
2026-04-16 17:36:40 +0530

    update

--- src/calculator.c
+++ src/calculator.c
@@ -8,13 +8,3 @@ int subtract(int a, int b) {
     return a - b;
 }
  
-int multiply(int a, int b) {
-    return a * b;
-}
- 
-float divide(int a, int b) {
-    if (b == 0) {
-        return 0; // simple handling
-    }
-    return (float)a / b;
-}
